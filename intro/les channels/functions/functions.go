8bf4fc55db7d26ba0638b7c5fa108afbe6a4b51f
valdoth <valdotsiarohasina@gmail.com>
2022-08-06 21:17:09 +0300

    intro to go

--- intro/les channels/functions/functions.go
+++ intro/les channels/functions/functions.go
@@ -1,8 +1,12 @@
 package functions
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+)
 
 func GenerateRandomNumber(n int) int  {
+	rand.Seed(time.Now().UnixNano())
 	randomNumber := rand.Intn(n)
 	return randomNumber
 }
--- intro/les channels/main.go
+++ intro/les channels/main.go
@@ -5,9 +5,21 @@ import (
 	"myprogram/functions"
 )
 
+func CalculateValue(intChan chan int) {
+	randomNumber := functions.GenerateRandomNumber(50)
+	intChan  <- randomNumber
+}
+
 func main() {
 	fmt.Println("Les channels")
 
 	foo := make(chan int)
-	
+	defer close(foo)
+
+	go CalculateValue(foo)
+
+	num := <-foo
+
+	fmt.Println(num)
+
 }
